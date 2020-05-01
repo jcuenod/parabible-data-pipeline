a08a1367f62981bb60b8d3e3d6387d57892de0c8
James Cuénod <j3frea+gitkraken@gmail.com>
2020-05-01 16:24:48 -0500

    Get iterators working

--- tf-bhs/main.js
+++ tf-bhs/main.js
@@ -1,53 +1,64 @@
 const fs = require("fs")
 const exec = require("child_process").execSync
 
-const word_features = require("./word_features.json")
-const outputFile = "./output/bhs.sqlite"
+const sqlOutputFile = `${__dirname}/output/bhs.sqlite`
+const jsonOutputFile = `${__dirname}/output/word_features.json`
 
 console.log("Checking for prepopulated sqlite")
-if (!fs.existsSync(outputFile)) {
+if (!fs.existsSync(sqlOutputFile) || !fs.existsSync(jsonOutputFile)) {
 	console.log(" - Does not exist, generating...")
 
 	let result
 	console.log(" - - Installing text-fabric")
 	result = exec("pip install text-fabric --user")
 	console.log(" - - Creating sqlite (will take a while)")
-	result = exec(`python ./1.\ create_sqlite_from_tf.py ${outputFile}`)
+	result = exec(`python './1. create_sql_from_tf.py' ${sqlOutputFile} ${jsonOutputFile}`)
 	// convert and show the output.
 	// console.log(result.toString("utf8");
 }
+else {
+	console.log(" - Found cached files!")
+}
 
-const sqlite = require('better-sqlite')
-const db = sqlite(outputFile)
+const word_features = require(jsonOutputFile)
+
+const sqlite = require('better-sqlite3')
+const db = sqlite(sqlOutputFile)
 
 const prepare_word_iterator = () => {
-	const row = db.prepare('SELECT * FROM words').all()
-	return function* word_iterator() {
-		row.forEach(w => {
-			yield w
-		})
+	const rows = db.prepare('SELECT * FROM words').all()
+	function* word_iterator() {
+		for (let i = 0; i < rows.length; i++) {
+			yield rows[i]
+		}
 	}
+	return word_iterator
 }
 const prepare_text_iterator = () => {
-	const rows = db.prepare('SELECT wid, word_in_text, trailer, rid FROM words ORDER BY wid').all()
+	const rows = db.prepare('SELECT wid, text, trailer, reference_node_id FROM words ORDER BY wid').all()
 	const rows_by_rid = {}
 	rows.forEach(row => {
-		if (!(row.rid in rows_by_rid)) {
-			rows_by_rid[row.rid] = []
+		rid = row.reference_node_id
+		if (!(rid in rows_by_rid)) {
+			rows_by_rid[rid] = []
 		}
-		const { wid, word_in_text, trailer } = row
-		rows_by_rid[row.rid].push([ wid, word_in_text, trailer ])
+		const { wid, text, trailer } = row
+		rows_by_rid[rid].push([ wid, text, trailer ])
 	})
-	return function* text_iterator() {
-		Object.keys(rows_by_rid).forEach(key => {
+	function* text_iterator() {
+		const rows_by_rid_keys = Object.keys(rows_by_rid) 
+		for (let i = 0; i < rows_by_rid_keys.length; i++) {
+			const key = rows_by_rid_keys[i]
 			yield { rid: key, text: rows_by_rid[key] }
-		})
+		}
 	}
+	return text_iterator
 }
 module.exports = {
 	word_features: () => word_features,
+	ti: () => prepare_text_iterator(),
 	text_iterator: (parallel_id_object) => {
 		parallel_id_object = {bhs, lxx, kjv}
-	}
+	},
 	word_iterator: prepare_word_iterator()
 }
