1d6b8eb349aec23f3fa316b51fcb4ea7cce86ba3
James Cuénod <j3frea+gitkraken@gmail.com>
2020-07-24 19:24:28 -0500

    More renaming (that's how you really make progress after all)

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-const bhs = require("./tf-bhs/main.js")
+const bhs = require("./hb-bhs-pipe/main.js")
 
 
-const words = bhs.word_iterator()
+const words = bhs.word_iterator(0)
 for (w of words) {
 	console.log(w)
 }
