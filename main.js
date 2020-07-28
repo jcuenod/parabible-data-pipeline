ad790c0caa21740e08ce3121be4b6a9b2ee9ed6a
James Cuénod <j3frea+gitkraken@gmail.com>
2020-07-28 00:41:28 -0500

    Remember to call the iterator and to instantiate with named parameter

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const bhs = require("./hb-bhs-pipe/main.js")
 
 
-const words = bhs.word_iterator(0)
-for (w of words) {
+const words = bhs.word_iterator({ node_offset: 0 })
+for (let w of words()) {
 	console.log(w)
 }
