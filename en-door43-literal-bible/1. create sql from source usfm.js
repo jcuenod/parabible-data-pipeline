d330c5182ab96475ab104467c246cfb339b47850
James Cuénod <4253884+jcuenod@users.noreply.github.com>
2021-02-12 10:02:47 -0600

    Update 1. create sql from source usfm.js
    Add usfm2osis note

--- en-door43-literal-bible/1. create sql from source usfm.js
+++ en-door43-literal-bible/1. create sql from source usfm.js
@@ -1,2 +1,4 @@
 // If no source-repository:
 const clone_command = `git clone https://git.door43.org/unfoldingWord/en_ult.git source-repository`
+
+// maybe it will be easier to work with osis... https://github.com/chrislit/usfm2osis
