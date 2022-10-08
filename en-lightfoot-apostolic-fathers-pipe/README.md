e260c0756076eb4419a347bee55f491d834feeeb
James Cuénod <4253884+jcuenod@users.noreply.github.com>
2022-10-07 22:03:31 -0500

    Initial commit (import lightfoot tr of apostolic fathers)

--- en-lightfoot-apostolic-fathers-pipe/README.md
+++ en-lightfoot-apostolic-fathers-pipe/README.md
@@ -1,7 +1,20 @@
-possible sources:
- - https://ccat.sas.upenn.edu/gopher/text/religion/churchwriters/ApostolicFathers/ (CCAT licensing...?!? but it has italics and clearer chapter/verse divisions)
- - https://ccel.org/ccel/l/lightfoot/fathers/cache/fathers.txt (clear copyright in file header)
+# Data Source
 
- for glosses and parsing, consider:
- 1. https://scaife.perseus.org/morpheus/?word=πατρὸς&lang=grc
- 1. https://vocab.perseus.org/lemma/json/?l=πατήρ (from result above)
+| | Notes |
+| --- | --- |
+| **Content** | Apostolic Fathers (English tr. by Lightfoot) |
+| **Source** | <https://ccat.sas.upenn.edu/gopher/text/religion/churchwriters/ApostolicFathers/> |
+| **Format** | Plain Text |
+| **License** | Public Domain (?) |
+
+## Content
+
+### Apostolic Fathers
+
+This corpus provides insight into both Greek language and early Christian theology. From Wikipedia:
+
+> The Apostolic Fathers, also known as the Ante-Nicene Fathers, were core Christian theologians among the Church Fathers who lived in the 1st and 2nd centuries AD, who are believed to have personally known some of the Twelve Apostles, or to have been significantly influenced by them. Their writings, though widely circulated in Early Christianity, were not included in the canon of the New Testament. Many of the writings derive from the same time period and geographical location as other works of early Christian literature which came to be part of the New Testament. Some of the writings found among the Apostolic Fathers appear to have been as highly regarded as some of the writings which became the New Testament. (https://en.wikipedia.org/wiki/Apostolic_Fathers)
+
+### License
+
+The license from CCAT is unclear. However, the lightfoot translation is available from CCEL at <https://ccel.org/ccel/l/lightfoot/fathers/cache/fathers.txt>. In the header of that file, the license is listed as public domain.
---
+++ en-lightfoot-apostolic-fathers-pipe/main.js
@@ -0,0 +1,109 @@
+const sqlite = require("better-sqlite3")
+const INSERT_LIMIT = 5000
+
+const fs = require("fs")
+const ReferenceParser2 = require("/home/jcuenod/Programming/ReferenceParser/lib/index.js")
+const rp = new ReferenceParser2.default()
+
+const RID_OFFSET = 111
+const BOOK_ORDER = [
+    "Barnabas",
+    "1 Clement",
+    "2 Clement",
+    "Didache",
+    "Diognetus",
+    "Shepherd of Hermas",
+    "Ignatius to the Ephesians",
+    "Ignatius to the Magnesians",
+    "Ignatius to the Phila",
+    "Ignatius to Polycarp",
+    "Ignatius to the Romans",
+    "Ignatius to the Philadelphians",
+    "Ignatius to the Smyrneans",
+    "Ignatius to the Trallians",
+    "Martyrdom of Polycarp",
+    "Polycarp to the Philippians",
+]
+
+const refToRid = ref => {
+    const book_int = RID_OFFSET + BOOK_ORDER.indexOf(ref.book)
+    return book_int * 1000000 +
+        ref.chapter * 1000 +
+        ref.verse
+}
+
+const outputDb = new sqlite("./output/data.sqlite")
+outputDb.exec(`
+DROP TABLE IF EXISTS verse_text;`)
+outputDb.exec(`
+CREATE TABLE verse_text (
+	rid INTEGER,
+	text TEXT
+);`)
+
+const escape = str => str.replace(/'/g, "''")
+const generateInsertStatement = values => `
+INSERT INTO verse_text VALUES 
+${values.map(v =>
+    `(${v[0]}, '${escape(v[1])}')`
+).join(",")}`
+
+
+const IGNORE_LIST = [
+    "README.md",
+    ".git",
+    // TODO: Just for now
+    "Hermas"
+]
+
+const files = fs.readdirSync("./source-files").filter(f => !IGNORE_LIST.includes(f))
+
+console.log("Parsing Files:")
+const toInsert = []
+files.forEach(f => {
+    console.log(" -", f)
+    const paragraphsWithHeader = fs.readFileSync(`./source-files/${f}`, "utf8").split("\n\n").filter(p => !!p)
+    const paragraphs = paragraphsWithHeader.slice(1)
+
+    paragraphs.forEach((p, i) => {
+        const lines = p.trim().split("\n")
+        const ref = lines[0].endsWith("prologue:1")
+            ? rp.parse(lines[0].slice(0, lines[0].length - "prologue:1".length))
+            : rp.parse(lines[0])
+        if (lines[0].endsWith("prologue:1")) {
+            ref.verse = 1
+        }
+        const rid = refToRid(ref)
+        if (i === 0) {
+            console.log(" - ", ref, rid)
+        }
+        const content = lines.slice(1).join(" ")
+
+        if (rid === 110000000 || Number.isNaN(rid)) {
+            console.error(`Could not parse ${lines[0]}`)
+            throw ("nan")
+        }
+
+        toInsert.push([
+            rid,
+            content.trim()
+        ])
+    })
+})
+console.log(" - Done")
+
+while (toInsert.length > 0) {
+    const doInsert = toInsert.splice(0, INSERT_LIMIT)
+    const stmt = generateInsertStatement(doInsert)
+    outputDb.exec(stmt)
+}
+
+const module_data = {
+    "name": "Apostolic Fathers (Lightfoot's Translation)",
+    "abbreviation": "ApFathersEn",
+    "versification_schema": "kjv",
+    "license": "Adapted and Modified © 1990 ATHENA DATA PRODUCTS",
+    "url": "https://ccat.sas.upenn.edu/gopher/text/religion/churchwriters/ApostolicFathers/",
+    "language": "en"
+}
+fs.writeFileSync("./output/module.json", JSON.stringify(module_data), "utf-8")
